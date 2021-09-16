a5348a678db59fb08b98865dd713888e8a995abc
Kumail Raza <46721070+kumailispro@users.noreply.github.com>
2021-09-15 22:33:01 -0400

    changed help message

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,12 +3,12 @@ const Command = require("../structures/Command.js");
 const PREFIX = process.env.BETTER_FF_BOT_PREFIX;
 module.exports = new Command({
     name: "help",
-    description: "Provides information to the user regarding the bot and its commands.",
+    description: "Provides informational overview of commands.",
     async run(message, args, client) {
         const helpEmbed = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Help Menu')
-            .setDescription('This is the help menu where you can get a list of all the commands that can be performed using the bot as well as what they do.')
+            .setDescription('To get more details on how to use a command, try using it without any arguments.')
             .setTimestamp()
             .setFooter('Bot by kum#9965');
         
